9cb4ea67a167b2cb71558924f704fc3f474ca132
brownynik <browny@yandex.ru>
2018-07-24 22:42:52 +0300

    Add files via upload

--- udp2mqtt.go
+++ udp2mqtt.go
@@ -8,7 +8,11 @@ import (
 	"encoding/json"
 	"github.com/eclipse/paho.mqtt.golang"
 	"flag"
-
+	"container/list"
+	"strings"
+	"crypto/cipher"
+	"crypto/aes"
+	"encoding/hex"
 )
 
 /*
@@ -23,6 +27,113 @@ Options:
  [-topic <topic>]             Topic
  [-cfgfile <filename>]        Filename with filter of sensors and other options
 */
+
+var magicKey = []byte{ 0x17, 0x99, 0x6d, 0x09, 0x3d, 0x28, 0xdd, 0xb3, 0xba, 0x69, 0x5a, 0x2e, 0x6f, 0x58, 0x56, 0x2e }
+
+const localeTimeZone = "Europe/Moscow"
+
+type xiaomiDeviceIntf interface {
+    GetSID() string
+    GetName() string
+    SetSID(SID string)
+    SetName(Name string)
+    GetLastTime() time.Time
+    SetLastTime(LastTime time.Time)
+    GetModel() string
+    SetModel(Model string)
+}
+
+type XiaomiDevice struct {
+
+    SID			string
+    Name		string
+    Model		string
+    LastTime		time.Time
+    xiaomidevice	xiaomiDeviceIntf
+
+}
+
+func (d *XiaomiDevice) GetSID() string { return d.SID }
+
+func (d *XiaomiDevice) GetName() string { return d.Name }
+
+func (d *XiaomiDevice) SetName(Name string) { d.Name = Name }
+
+func (d *XiaomiDevice) SetSID(SID string) { d.SID = SID }
+
+func (d *XiaomiDevice) GetLastTime() time.Time { return d.LastTime }
+
+func (d *XiaomiDevice) SetLastTime(LastTime time.Time) { d.LastTime = LastTime }
+
+func (d *XiaomiDevice) GetModel() string { return d.Model }
+
+func (d *XiaomiDevice) SetModel(Model string) { d.Model = Model }
+
+
+
+type tSensor struct{
+    XiaomiDevice
+    Voltage		float32
+}
+
+type tGateway struct{
+    XiaomiDevice
+    IPAddress		string
+    Password		string
+    Token		string
+    SecureKey		string
+}
+
+func (gw *tGateway) RecalcSecureKey(Token string) string { 
+
+    
+    block, err := aes.NewCipher([]byte(gw.Password))
+
+    if err != nil {
+	panic(err)
+    }
+
+    var ciphertext = make([]byte, 16)
+
+    var mode = cipher.NewCBCEncrypter(block, magicKey)
+
+    mode.CryptBlocks(ciphertext, []byte(Token))
+
+    gw.Token = Token
+
+    gw.SecureKey = hex.EncodeToString(ciphertext)
+
+    return gw.SecureKey
+    
+}
+
+
+func (gw *tGateway) GetSecureKey() string { return gw.SecureKey }
+
+
+type XiaomiList struct {
+    list.List
+}
+
+
+func (l *XiaomiList) DeviceBySID(SID string) *list.Element {
+
+
+    if l.Len() == 0 {
+	return nil
+    }
+
+
+    for e:= l.Front(); e != nil; e = e.Next() {
+	
+	if e.Value.(xiaomiDeviceIntf).GetSID() == SID {
+	    return e
+	}
+
+    }
+
+    return nil
+}
 
 
 type Config struct {
@@ -40,6 +151,8 @@ type Config struct {
 }
 
 var cfg = Config{}
+
+var lstDevices = new(XiaomiList)
 
 var mqttClient mqtt.Client
 
@@ -79,8 +192,52 @@ func init() {
 	_, err = file.Read(bs)
 	checkError(err)
 	
-	err = json.Unmarshal(bs, &cfg.lstSensors)
+
+
+	var mapXiaomiDevices map[string]interface{}
+	err = json.Unmarshal(bs, &mapXiaomiDevices)
 	checkError(err)
+
+	// ищем в настройках gateways
+	
+	if _, ok:= mapXiaomiDevices["gateways"]; ok {
+	    //fmt.Println(keyname, ok)
+
+
+	    
+	for gatewaySID, gatewayPassword:= range mapXiaomiDevices["gateways"].(map[string]interface{}) {
+
+	    lstDevices.PushBack(&tGateway{Password: gatewayPassword.(string), XiaomiDevice: XiaomiDevice{SID: gatewaySID, Model: "gateway"}})
+	}
+
+	for deviceSID, deviceName:= range mapXiaomiDevices {
+	    
+	    if deviceSID!="gateways" {
+
+		var itm *list.Element
+		itm = lstDevices.DeviceBySID(deviceSID)
+
+		if itm!= nil {
+		
+		    itm.Value.(xiaomiDeviceIntf).SetName(deviceName.(string))
+		
+		} else {
+		
+		    lstDevices.PushBack(&tSensor{XiaomiDevice: XiaomiDevice{Name: deviceName.(string), SID: deviceSID}})
+		}
+	    }
+
+	}
+
+
+
+
+
+
+	}
+
+
+
 
     }
 
@@ -133,64 +290,133 @@ func sendMQTTMessage(channel chan string, ) {
 	
 	// fmt.Println("Msg = ", msg)
 
+	
+	// сразу и без промедления отправляем MQTT сообщение
+	if publish := mqttClient.Publish(*cfg.mqttTopic, byte(*cfg.mqttQos), false, msg); publish.Wait() && publish.Error() != nil {
+		fmt.Println(publish.Error())
+	}
+
+	// а теперь парсим и т.п.
+
 	var payload map[string]interface{}
 	if err := json.Unmarshal([]byte(msg), &payload); err != nil {
 	    continue
 	}
 
+	
+	var devName string
+	devSID:= payload["sid"].(string)
+	devModel:= payload["model"].(string)
+	currentTime:= time.Now()
+	var dev_cmd string = ""
+	_ = dev_cmd
 
-	if (cfg.lstSensors == nil)||((payload["sid"]!=nil) && (cfg.lstSensors[payload["sid"].(string)]!=nil)) {
+	if payload["cmd"]!= nil {
+	    dev_cmd = payload["cmd"].(string)
+	}
+
+
+	var devToken string = ""
+	var devIPAddress string= ""
+	var devVoltage float32 = 0
+
+	_ = devToken
+	_ = devIPAddress
+	_ = devVoltage
+
+	if devModel == "gateway" {
 	    
-	    sensorName := "noname"
-	    if (cfg.lstSensors!= nil) {
-		sensorName = cfg.lstSensors[payload["sid"].(string)].(string)
+	    if payload["token"]!=nil {
+		devToken = payload["token"].(string)
 	    }
 
-	    fmt.Println("Translate data for sensor", sensorName)
+	    if payload["data"]!= nil {
 
-	    
-	    
-	    // check for gateway
-	    /*
-	    if (payload["model"]!=nil)&&(payload["model"].(string) == "gateway") {
-
-		// create gateways interface
-		if cfg.lstGateways == nil {
-		    // future functionality...
 		
+		var pdata_json map[string]interface{}
+
+		if err := json.Unmarshal([]byte(payload["data"].(string)), &pdata_json); err != nil {
+		    continue
+		}		    
+		
+
+		if pdata_json["ip"]!=nil {
+
+		    devIPAddress = pdata_json["ip"].(string)
 		}
 
-	    }
-	    */
-
-	    if publish := mqttClient.Publish(*cfg.mqttTopic, byte(*cfg.mqttQos), false, msg); publish.Wait() && publish.Error() != nil {
-		fmt.Println(publish.Error())
+		
 	    }
 	    
-	    fmt.Println("Msg = ", msg)
+
+	} else
+	{
+	    if payload["Voltage"]!= nil {
+		devVoltage = payload["Voltage"].(float32)
+	    }
 
 	}
+
+
+	// ищем в списке item по заданному в udp пакете SID
+	var itm *list.Element
+	itm = lstDevices.DeviceBySID(devSID)
+	if itm!= nil {
+	    
+	    if itm.Value.(xiaomiDeviceIntf).GetName() == "" {
+		itm.Value.(xiaomiDeviceIntf).SetName("Unknown" + strings.Title(devModel) + devSID)
+	    }
+
+	    if itm.Value.(xiaomiDeviceIntf).GetModel() == "" {
+		itm.Value.(xiaomiDeviceIntf).SetModel(devModel)
+	    }
+	    
+	    itm.Value.(xiaomiDeviceIntf).SetLastTime(currentTime)
+
+	    if devModel == "gateway" {
+				
+		// itm.Value.(*tGateway).Token = devToken
+
+		itm.Value.(*tGateway).IPAddress = devIPAddress
+
+		if devToken!="" && itm.Value.(*tGateway).Password!="" {
+
+		    var sKey = itm.Value.(*tGateway).RecalcSecureKey(devToken)
+
+		    fmt.Printf("SID = %s, IPAddress = %s, Token = %s, SecureKey = %s\n\r", devSID, devIPAddress, devToken, sKey)
+
+
+		}
+
+
+	    } else {
+		
+		itm.Value.(*tSensor).Voltage = devVoltage
+
+	    }
+
+
+	    
+	} else // itm не найден - это новый элемент!
+	{
+	    devName = "Unknown" + strings.Title(devModel) + devSID
+	    if devModel == "gateway" {
+
+		lstDevices.PushBack(&tGateway{Token: devToken, IPAddress: devIPAddress, XiaomiDevice: XiaomiDevice{Name: devName, SID: devSID, Model: devModel, LastTime: currentTime}})
+
+		// увы, тут нет смысла расчитывать SecureKey, т.к. устройство добавлено автоматически и не имеет пароля из настройки
+
+	    } else {
+		lstDevices.PushBack(&tSensor{Voltage: devVoltage, XiaomiDevice: XiaomiDevice{Name: devName, SID: devSID, Model: devModel, LastTime: currentTime}})
+	    }
+	}
+
 
   }
 }
 
 
 func main() {
-
-
-/*
-    opts := mqtt.NewClientOptions().AddBroker("tcp://192.168.1.10:1883").SetClientID("mqtt-proxy")
-    opts.SetKeepAlive(2 * time.Second)
-    opts.SetUsername("esp8266")
-    opts.SetPassword("rP09-x27mB3g")
-    opts.SetCleanSession(true)
-
-    mqttClient := mqtt.NewClient(opts)
-    if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
-	panic(token.Error())
-    }
-*/
-
 
 	conn, err := net.ListenMulticastUDP("udp", nil, &net.UDPAddr{IP: net.IPv4(224, 0, 0, 50), Port: 9898})
 	checkError(err)
